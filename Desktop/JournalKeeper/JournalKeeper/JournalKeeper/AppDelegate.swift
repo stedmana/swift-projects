a1e433931c88d31e612742af2ff5db85c8e9c551
Alex Stedman <alexander.stedman@mail.mcgill.ca>
2018-04-10 22:36:24 -0400

    Started journal app

---
+++ Desktop/JournalKeeper/JournalKeeper/JournalKeeper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JournalKeeper
+//
+//  Created by Alex Stedman on 2018-04-10.
+//  Copyright © 2018 Alex Stedman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Desktop/JournalKeeper/JournalKeeper/JournalKeeper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JournalKeeper
+//
+//  Created by Alex Stedman on 2018-04-10.
+//  Copyright © 2018 Alex Stedman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
